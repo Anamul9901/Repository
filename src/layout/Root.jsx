10154071baa56f174377d61122e85a54325edcfd
Anamul9901 <anamulhaque9901@gmail.com>
2023-12-21 12:44:57 +0600

    navber added

---
+++ src/layout/Root.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Navber from "../components/Navber/Navber";
+import Footer from "../components/Footer/Footer";
+
+
+
+const Root = () => {
+    return (
+        <div>
+            <Navber />
+            <Outlet />
+            <Footer />
+        </div>
+    );
+};
+
+export default Root;
